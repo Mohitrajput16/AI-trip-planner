57cb870f945211d42bf544e9c562aeee90baa57d
Mohitrajput16 <mohitrajput1694@gmail.com>
2025-10-10 22:50:32 +0530

    feat: Implement user trip retrieval and display in MyTrips component

--- Trip-planner/src/my-trips/index.jsx
+++ Trip-planner/src/my-trips/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { db } from '@/service/firebaseConfig';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from 'react-router-dom';
+import UserTripCardItem from './components/UserTripCardItem';
 
-const MyTrips = () => {
+function MyTrips() {
+  const navigation = useNavigation();
+  const [userTrips,setUserTrips]=useState([]);
+
+  useEffect(() => {
+    GetUserTrips();
+  }, [])
+
+  const GetUserTrips =async () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+      navigation('/');
+      return;
+    }
+    const q=query(collection(db,'AITrips'),where('userEmail','==',user?.email));
+    const querySnashot=await getDocs(q);
+    querySnashot.forEach((doc)=>{
+      console.log(doc.id,"=>",doc.data());
+      setUserTrips(prevVal=>[...prevVal,doc.data()])
+    });
+  }
   return (
-    <div>
-      <h1>my trips</h1>
+    <div className='sm:px-10 md:px-32 lg:px-52 xl:px-72 px-5 mt-12'>
+      <h2 className='font-bold text-3xl '>My trips</h2>
+      <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
+        {userTrips.map((trip,index)=>(
+          <UserTripCardItem trip={trip} />
+        ))}
+      </div>
     </div>
   )
 }
