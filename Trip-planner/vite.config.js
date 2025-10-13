f9732d87d9032596c98537b302cbadf0d295ed4c
Mohitrajput16 <mohitrajput1694@gmail.com>
2025-10-13 22:15:21 +0530

    config resolved

--- Trip-planner/vite.config.js
+++ Trip-planner/vite.config.js
@@ -1,9 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from '@tailwindcss/vite'; // Correct import
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ["react-google-places-autocomplete"],
   },
