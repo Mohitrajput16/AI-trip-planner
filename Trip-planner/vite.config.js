c30265e029a7d34276965f5c425ff23d2507d123
Mohitrajput16 <mohitrajput1694@gmail.com>
2025-10-14 17:15:31 +0530

    Refactor: clean up vite.config.js and adjust react-google-places-autocomplete handling

--- Trip-planner/vite.config.js
+++ Trip-planner/vite.config.js
@@ -1,20 +1,19 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import tailwindcss from '@tailwindcss/vite'; // Correct import
-
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ["react-google-places-autocomplete"],
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom"],
   },
+  build: {
+    rollupOptions: {
+      external: ["react-google-places-autocomplete"],
+    },
+  },
 })
-
-
